ee02ddb9516f127719e8a5fdb6d64a063d1ef2a5
Andrea Passini <andreapa98@gmail.com>
2022-05-03 18:39:48 +0200

    LookAtMouse

---
+++ LetMeRIP/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Camera camera;
+
+    private Vector3 directionToLook;
+    private Rigidbody rb;
+
+    private Quaternion rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcolateAngle();
+    }
+
+	private void FixedUpdate()
+	{
+        Rotate();
+	}
+
+    private (bool success, Vector3 position) GetMousePosition()
+	{
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask)) 
+        {
+            // If hit something return the pos
+            return (success: true, position: hitInfo.point);
+        } 
+        else 
+        {
+            // If hit something return the pos
+            return (success: false, position: Vector3.zero);
+        }
+    }
+
+    private void CalcolateAngle()
+	{
+        var (success, position) = GetMousePosition();
+		if (success) 
+        {
+            // Calculate direction
+            directionToLook = position - transform.position;
+		}
+        
+    }
+
+    void Rotate()
+	{
+        // to keep the same hight
+        directionToLook.y = 0;
+
+        transform.forward = directionToLook;
+	}
+}

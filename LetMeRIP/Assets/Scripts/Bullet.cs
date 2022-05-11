c7f65feeed3728a6bdf61bd513321741bcb23def
Andrea Passini <andreapa98@gmail.com>
2022-05-11 20:46:50 +0200

    Enemy Health (Knock Back not workig yet)

--- LetMeRIP/Assets/Scripts/Bullet.cs
+++ LetMeRIP/Assets/Scripts/Bullet.cs
@@ -6,8 +6,9 @@ public class Bullet : MonoBehaviour
 {
 
     [SerializeField] GameObject hitDmgEffect;
-
     [SerializeField] float destroyAfterTime = 5f;
+    [SerializeField] float damage = 20f;
+
 
     void Start()
     {
@@ -17,11 +18,26 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("HIT: " + collision.gameObject.GetComponent<EnemyHealth>());
+
+        EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+
+        if(enemyHealth != null)
+        {
+            enemyHealth.OnDamage(damage, transform.position);
+        }
+
+        // NOT WORKING
+        //if (TryGetComponent (out EnemyHealth h))
+        //{
+        //    h.OnDamage(damage, transform.position);
+        //}
+
         //GameObject effect = Instantiate(hitDmgEffect, transform.position, Quaternion.identity);
         //Destroy(effect, 2f);
 
         //if(collision.collider.CompareTag("Enemy"))
-            Destroy(gameObject);
+        Destroy(gameObject);
     }
 
     public IEnumerator DestroyBulletAfterTime()
---
+++ LetMeRIP/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float maxHealth = 100f;
+    private float health;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        health = maxHealth;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnDamage(float dmg, Vector3 positionHit)
+    {
+        Debug.Log("Got HIT");
+
+        health -= dmg;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+
+        Vector3 fromHitToBody = (transform.position - positionHit).normalized;
+
+        rb.AddForce(fromHitToBody * dmg / 10, ForceMode.Impulse);
+    }
+
+    public void Die()
+    {
+        Object.Destroy(gameObject);
+    }
+}

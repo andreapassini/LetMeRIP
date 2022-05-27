b3cf35e72e92993b51e4ba2c1844211194441b2b
danielbalanica00 <danielbalanica.dev@gmail.com>
2022-05-27 11:21:44 +0200

    added basic main menu & lobby

---
+++ LetMeRIP/Assets/Scripts/Networking/ConnectToServer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    public GameObject loadingScreen;
+    public GameObject lobbyScreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        loadingScreen.SetActive(false);
+        lobbyScreen.SetActive(true);
+    }
+}
---
+++ LetMeRIP/Assets/Scripts/Networking/CreateAndJoinRooms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+    // Start is called before the first frame update
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+    
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+}

bc7b3fc58ce6116420070098de3df17625d8539c
danielbalanica00 <danielbalanica.dev@gmail.com>
2022-05-28 11:12:07 +0200

    re-implemented menu

---
+++ LetMeRIP/Assets/Scripts/Networking/PhotonLauncher.cs
@@ -0,0 +1,71 @@
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PhotonLauncher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private TMP_InputField roomNameInputField;
+    
+    private void Start()
+    {
+        Debug.Log("Connecting to Master Server");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("title");
+        Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text)) return;
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        
+        MenuManager.Instance.OpenMenu("loading");
+    }
+    
+    public void JoinRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text)) return;
+        PhotonNetwork.JoinRoom(roomNameInputField.text);
+        
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(1);
+        MenuManager.Instance.OpenMenu("joined");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed: " + message);
+        MenuManager.Instance.OpenMenu("title");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Join creation failed: " + message);
+        MenuManager.Instance.OpenMenu("lobby");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+    }
+}

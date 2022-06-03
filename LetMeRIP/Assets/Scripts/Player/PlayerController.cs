47ea1d6e8674643893b9c22a4c9a3929c3ddd108
Andrea Borghesi <00borghesiandrea@gmail.com>
2022-06-03 11:44:21 +0200

    player controller photon view accessibility fix

--- LetMeRIP/Assets/Scripts/Player/PlayerController.cs
+++ LetMeRIP/Assets/Scripts/Player/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private PhotonView photonView;
+    public PhotonView photonView;
     PlayerInputActions playerInputActions;
 
     [SerializeField] private string playerClass; // archer, mage or warrior

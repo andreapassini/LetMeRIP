e5e52e403fae96a06962860ff644320772dbe4da
Andrea Passini <andreapa98@gmail.com>
2022-06-11 16:40:54 +0200

    Squashed commit of the following:
    commit b9ef4bf50c28b670e310d284b1cdb0eb89daed10
    Author: Andrea Passini <andreapa98@gmail.com>
    Date:   Sat Jun 11 16:40:02 2022 +0200
    
        Spider attack fixed
    
    commit a4f5db4e0a27bc7ec63596b7fb38e432e581cb7c
    Author: Andrea Passini <andreapa98@gmail.com>
    Date:   Thu Jun 9 19:03:17 2022 +0200
    
        Boss animation fix
    
    commit 039c39820b6b1f2f20a0ddb916df8be91e6fcf0f
    Author: Andrea Passini <andreapa98@gmail.com>
    Date:   Thu Jun 9 18:23:13 2022 +0200
    
        Animation desabled rootmotion

--- LetMeRIP/Assets/Scripts/Abilities/Enemies/Scripts/HeavyAttackPhase3.cs
+++ LetMeRIP/Assets/Scripts/Abilities/Enemies/Scripts/HeavyAttackPhase3.cs
@@ -41,20 +41,18 @@ public class HeavyAttackPhase3 : EnemyAbility
 		base.PerformAbility(enemy);
 
 		// Creates overlap sphere
-		Collider[] hitEnemies = Physics.OverlapSphere(enemy.attackPoint.position, range, enemy.whatIsTarget);
+		Collider[] hitEnemies = Physics.OverlapSphere(enemy.attackPoint.position, range);
 
 		// Check for collision
 		foreach (Collider e in hitEnemies) {
-			Debug.Log("Hit this guy: " + e.name);
 
-			// HPManager
-			// Anche eventi
+			if (e.CompareTag("Player")) {
+				HPManager hpManager = e.transform.GetComponent<HPManager>();
 
-			HPManager hpManager = e.transform.GetComponent<HPManager>();
-
-			if (hpManager != null) {
-				hpManager.TakeDamage(damage + enemy.enemyStats.attack, enemy.transform.position);
-			}
+				if (hpManager != null) {
+					hpManager.TakeDamage(damage + enemy.enemyStats.attack, enemy.transform.position);
+				}	
+			}			
 		}
 
 	}

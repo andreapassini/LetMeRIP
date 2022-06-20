75f9b51a4553ca7aa2646f19f84d12c987c91445
danielbalanica00 <danielbalanica.dev@gmail.com>
2022-06-20 20:54:11 +0200

    fully implemented dialogues

--- LetMeRIP/Assets/PlayerHudController.cs
+++ LetMeRIP/Assets/PlayerHudController.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class PlayerHudController : MonoBehaviour
+public class PlayerHudController : MonoBehaviourPun
 {
+    private PlayerController playerController;
+    
+    public void Start()
+    {
+        if (!photonView.IsMine) return;
+        playerController = transform.GetComponentInParent<PlayerController>();
+    }
+    
     public void Hide() => HudController.Instance.Hide();
     public void Show() => HudController.Instance.Show();
+
+    public void DisableMovement() => playerController.DisableAll();
+    public void EnableMovement() => playerController.EnableAll();
 }
